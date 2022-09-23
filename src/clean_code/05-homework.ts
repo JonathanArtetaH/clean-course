b183e18e2e27eb4189b26d35eea5dc57103aa74a
Jonathan AH <54410480+JonathanArtetaH@users.noreply.github.com>
2022-09-23 14:21:00 -0500

    Seccion 1

---
+++ src/clean_code/05-homework.ts
@@ -0,0 +1,61 @@
+(() => {
+
+
+    // Resolver sin la triple condicional dentro del if
+    // includes? arrays?
+    function isRedFruit( fruit: string ): boolean {
+        const redFruits =['manzana','cereza','ciruela']
+
+        return redFruits.includes(fruit);
+    
+    }
+
+    // Simplificar esta función
+    // switch? Object literal? validar posibles colores
+    function getFruitsByColor( color: string ): string[] { 
+        if ( color.includes('red' ))  return ['manzana','fresa']; 
+        
+        if ( color.includes('yellow')) return ['piña','banana'];
+        
+        if ( color.includes('purple')) return ['moras','uvas'];  
+
+        throw Error('the color must be: red, yellow, purple');
+    }
+
+    // Simplificar esta función
+    let isFirstStepWorking  = true;
+    let isSecondStepWorking = true;
+    let isThirdStepWorking  = true;
+    let isFourthStepWorking = true;
+
+    function workingSteps() {
+        if(!isFirstStepWorking === true ) return 'First step broken.';
+        
+        if(!isSecondStepWorking === true )return 'Second step broken.'; 
+        
+        if(!isThirdStepWorking === true ) return 'Third step broken.';
+        
+        if(!isFourthStepWorking === true) return 'Fourth step broken.';
+                
+        return 'Working properly!';
+            
+    }
+
+
+    // isRedFruit
+    console.log({ isRedFruit: isRedFruit('cereza'), fruit: 'cereza' }); // true
+    console.log({ isRedFruit: isRedFruit('piña'), fruit: 'piña' }); // true
+
+    //getFruitsByColor
+    console.log({ redFruits: getFruitsByColor('red') }); // ['manzana', 'fresa']
+    console.log({ yellowFruits: getFruitsByColor('yellow') }); // ['piña', 'banana']
+    console.log({ purpleFruits: getFruitsByColor('purple') }); // ['moras', 'uvas']
+    // console.log({ pinkFruits: getFruitsByColor('pink') }); // Error: the color must be: red, yellow, purple
+
+    // workingSteps
+    console.log({ workingSteps: workingSteps() }); // Cambiar los valores de la línea 31 y esperar los resultados
+
+
+})();
+
+
